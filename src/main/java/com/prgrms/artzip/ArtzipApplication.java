4d8f3f6bea17cbcaa8a9424eb3cdd4f978c3bba7
hong yuseok <jericoh@naver.com>
2022-07-25 18:59:55 +0900

    first commit

---
+++ src/main/java/com/prgrms/artzip/ArtzipApplication.java
@@ -0,0 +1,13 @@
+package com.prgrms.artzip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtzipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtzipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prgrms/artzip/ArtzipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prgrms.artzip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtzipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
