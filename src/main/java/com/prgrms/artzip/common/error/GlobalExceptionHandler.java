8c05b8409396458896df8b6dcdf6ab068709a3f1
park0503 <hooo0503@gmail.com>
2022-07-28 03:06:11 +0900

    chore: GlobalExceptionHandler 중복 코드 최적화

--- src/main/java/com/prgrms/artzip/common/error/GlobalExceptionHandler.java
+++ src/main/java/com/prgrms/artzip/common/error/GlobalExceptionHandler.java
@@ -27,73 +27,55 @@ public class GlobalExceptionHandler {
   // 500 : Internal Server Error
   @ExceptionHandler(Exception.class)
   public ResponseEntity<ErrorResponse> handleServerException(Exception e) {
-    log.warn(e.getMessage(), e);
-    ErrorResponse errorResponse = ErrorResponse.of(ErrorCode.INTERNAL_SERVER_ERROR);
-    return ResponseEntity.status(errorResponse.getStatus()).body(errorResponse);
+    return handleException(e, ErrorCode.INTERNAL_SERVER_ERROR);
   }
 
   // 405 : Method Not Allowed
   @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
   protected ResponseEntity<ErrorResponse> handleHttpRequestMethodNotSupportedException(
       HttpRequestMethodNotSupportedException e) {
-    log.error(e.getMessage(), e);
-    ErrorResponse errorResponse = ErrorResponse.of(ErrorCode.METHOD_NOT_ALLOWED);
-    return ResponseEntity.status(errorResponse.getStatus()).body(errorResponse);
+    return handleException(e, ErrorCode.METHOD_NOT_ALLOWED);
   }
 
   // 400 : MethodArgumentNotValidException
   @ExceptionHandler(MethodArgumentNotValidException.class)
   public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(
       MethodArgumentNotValidException e) {
-    log.warn(e.getMessage(), e);
-    ErrorResponse errorResponse = ErrorResponse.of(ErrorCode.INVALID_INPUT_VALUE);
-    return ResponseEntity.status(errorResponse.getStatus()).body(errorResponse);
+    return handleException(e, ErrorCode.INVALID_TYPE_VALUE);
   }
 
   // 400 : MethodArgumentType
   @ExceptionHandler(value = MethodArgumentTypeMismatchException.class)
   public ResponseEntity<ErrorResponse> handleMethodArgumentTypeMismatchException(
       MethodArgumentTypeMismatchException e) {
-    log.error(e.getMessage(), e);
-    ErrorResponse errorResponse = ErrorResponse.of(ErrorCode.INVALID_TYPE_VALUE);
-    return ResponseEntity.status(errorResponse.getStatus()).body(errorResponse);
+    return handleException(e, ErrorCode.INVALID_TYPE_VALUE);
   }
 
   // 400 : Bad Request, ModelAttribute
   @ExceptionHandler(org.springframework.validation.BindException.class)
   public ResponseEntity<ErrorResponse> handleBindException(BindException e) {
-    log.warn(e.getMessage(), e);
-    ErrorResponse errorResponse = ErrorResponse.of(ErrorCode.INVALID_INPUT_VALUE);
-    return ResponseEntity.status(errorResponse.getStatus()).body(errorResponse);
+    return handleException(e, ErrorCode.INVALID_INPUT_VALUE);
   }
 
   @ExceptionHandler(HttpMessageNotReadableException.class)
   public ResponseEntity<ErrorResponse> handleHttpMessageNotReadableException(
       HttpMessageNotReadableException e) {
-    log.warn(e.getMessage(), e);
-    ErrorResponse errorResponse = ErrorResponse.of(ErrorCode.INVALID_INPUT_VALUE);
-    return ResponseEntity.status(errorResponse.getStatus()).body(errorResponse);
+    return handleException(e, ErrorCode.INVALID_INPUT_VALUE);
   }
 
   @ExceptionHandler(IllegalArgumentException.class)
   public ResponseEntity<ErrorResponse> handleIllegalArgumentException(IllegalArgumentException e) {
-    log.warn(e.getMessage(), e);
-    ErrorResponse errorResponse = ErrorResponse.of(ErrorCode.INVALID_INPUT_VALUE);
-    return ResponseEntity.status(errorResponse.getStatus()).body(errorResponse);
+    return handleException(e, ErrorCode.INVALID_INPUT_VALUE);
   }
 
   @ExceptionHandler(NoHandlerFoundException.class)
   public ResponseEntity<ErrorResponse> handleNoHandlerFoundException(NoHandlerFoundException e) {
-    log.warn(e.getMessage(), e);
-    ErrorResponse errorResponse = ErrorResponse.of(ErrorCode.NOT_FOUND);
-    return ResponseEntity.status(errorResponse.getStatus()).body(errorResponse);
+    return handleException(e, ErrorCode.NOT_FOUND);
   }
 
   @ExceptionHandler(MissingServletRequestParameterException.class)
   public ResponseEntity<ErrorResponse> handleMissingServletRequestParameterException(MissingServletRequestParameterException e) {
-    log.warn(e.getMessage(), e);
-    ErrorResponse errorResponse = ErrorResponse.of(ErrorCode.MISSING_REQUEST_PARAMETER);
-    return ResponseEntity.status(errorResponse.getStatus()).body(errorResponse);
+    return handleException(e, ErrorCode.MISSING_REQUEST_PARAMETER);
   }
 
   /**
@@ -101,52 +83,44 @@ public class GlobalExceptionHandler {
    */
   @ExceptionHandler(NotFoundException.class)
   public ResponseEntity<ErrorResponse> handleNotFoundException(NotFoundException e) {
-    log.warn(e.getMessage(), e);
-    ErrorResponse errorResponse = ErrorResponse.of(e.getErrorCode());
-    return ResponseEntity.status(errorResponse.getStatus()).body(errorResponse);
+    return handleException(e, e.getErrorCode());
   }
 
   @ExceptionHandler(AlreadyExistsException.class)
   public ResponseEntity<ErrorResponse> handleFriendExistsException(AlreadyExistsException e) {
-    log.warn(e.getMessage(), e);
-    ErrorResponse errorResponse = ErrorResponse.of(e.getErrorCode());
-    return ResponseEntity.status(errorResponse.getStatus()).body(errorResponse);
+    return handleException(e, e.getErrorCode());
   }
 
   @ExceptionHandler(DuplicateRequestException.class)
   public ResponseEntity<ErrorResponse> handleDuplicateFriendRequestException(
       DuplicateRequestException e) {
-    log.warn(e.getMessage(), e);
-    ErrorResponse errorResponse = ErrorResponse.of(e.getErrorCode());
-    return ResponseEntity.status(errorResponse.getStatus()).body(errorResponse);
+    return handleException(e, e.getErrorCode());
   }
 
   @ExceptionHandler(InvalidRequestException.class)
   public ResponseEntity<ErrorResponse> handleInvalidRequestException(InvalidRequestException e) {
-    log.warn(e.getMessage(), e);
-    ErrorResponse errorResponse = ErrorResponse.of(e.getErrorCode());
-    return ResponseEntity.status(errorResponse.getStatus()).body(errorResponse);
+    return handleException(e, e.getErrorCode());
   }
 
   @ExceptionHandler(AuthErrorException.class)
   public ResponseEntity<ErrorResponse> handleAuthErrorException(AuthErrorException e) {
-    log.warn(e.getMessage(), e);
-    ErrorResponse errorResponse = ErrorResponse.of(e.getErrorCode());
-    return ResponseEntity.status(errorResponse.getStatus()).body(errorResponse);
+    return handleException(e, e.getErrorCode());
   }
 
   @ExceptionHandler(PermissionDeniedException.class)
   public ResponseEntity<ErrorResponse> handlePermissionDeniedException(
       PermissionDeniedException e) {
-    log.warn(e.getMessage(), e);
-    ErrorResponse errorResponse = ErrorResponse.of(e.getErrorCode());
-    return ResponseEntity.status(errorResponse.getStatus()).body(errorResponse);
+    return handleException(e, e.getErrorCode());
   }
 
   @ExceptionHandler(CannotSendMessageException.class)
   public ResponseEntity<ErrorResponse> handleCannotSendMessageException(CannotSendMessageException e) {
+    return handleException(e, e.getErrorCode());
+  }
+
+  private ResponseEntity<ErrorResponse> handleException(Exception e, ErrorCode errorCode) {
     log.warn(e.getMessage(), e);
-    ErrorResponse errorResponse = ErrorResponse.of(e.getErrorCode());
+    ErrorResponse errorResponse = ErrorResponse.of(errorCode);
     return ResponseEntity.status(errorResponse.getStatus()).body(errorResponse);
   }
 }
--- src/main/java/com/prgrms/artzip/common/error/exception/CannotSendMessageException.java
+++ src/main/java/com/prgrms/artzip/common/error/exception/CannotSendMessageException.java
@@ -1,7 +1,6 @@
 package com.prgrms.artzip.common.error.exception;
 
 import com.prgrms.artzip.common.ErrorCode;
-import com.prgrms.coretime.common.ErrorCode;
 import lombok.Getter;
 
 @Getter
