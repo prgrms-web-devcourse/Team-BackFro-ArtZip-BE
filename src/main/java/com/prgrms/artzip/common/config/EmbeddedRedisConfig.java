83c3b99f91bc1837b3fd3b3433199b16c587e0de
julie0005 <julie0005@ajou.ac.kr>
2022-07-28 06:26:32 +0900

    test: push test

--- src/main/java/com/prgrms/artzip/common/config/EmbeddedRedisConfig.java
+++ src/main/java/com/prgrms/artzip/common/config/EmbeddedRedisConfig.java
@@ -1,2 +1,33 @@
-package com.prgrms.artzip.common.config;public class EmbeddedRedisConfig {
+package com.prgrms.artzip.common.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import redis.embedded.RedisServer;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import java.io.IOException;
+
+@Configuration
+@Profile("test")
+public class EmbeddedRedisConfig {
+    //TODO: 내장 Redis Port 자동 설정
+    @Value("${spring.redis.port}")
+    private int port;
+
+    private RedisServer redisServer;
+
+    @PostConstruct
+    public void redisServer() throws IOException {
+        redisServer = new RedisServer(port);
+        redisServer.start();
+    }
+
+    @PreDestroy
+    public void stopRedis() {
+        if (redisServer != null && redisServer.isActive()) {
+            redisServer.stop();
+        }
+    }
 }
