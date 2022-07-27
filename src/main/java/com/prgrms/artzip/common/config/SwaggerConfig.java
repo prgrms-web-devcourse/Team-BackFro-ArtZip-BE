eb313e5d0f2fe07fd4678952aae0eee4274bd122
parksang <hooo0503@gmail.com>
2022-07-27 18:45:45 +0900

    chore: swagger 보안 세팅

--- src/main/java/com/prgrms/artzip/common/config/SwaggerConfig.java
+++ src/main/java/com/prgrms/artzip/common/config/SwaggerConfig.java
@@ -1,12 +1,17 @@
 package com.prgrms.artzip.common.config;
 
+import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
@@ -16,6 +21,8 @@ public class SwaggerConfig {
   public Docket apiV1() {
     return new Docket(DocumentationType.SWAGGER_2)
         .apiInfo(apiInfo())
+        .securitySchemes(List.of(accessToken(), refreshToken()))
+        .securityContexts(List.of(securityContext()))
         .select()
         .apis(RequestHandlerSelectors.any())
         .paths(PathSelectors.ant("/api/v1/**"))
@@ -28,5 +35,23 @@ public class SwaggerConfig {
         .description("Descriptions of ArtZip API - Programmers Devcourse Team BackFro")
         .version("1.0")
         .build();
+  }
+
+  private ApiKey accessToken() {
+    return new ApiKey("AccessJWT", "accessToken", "header");
+  }
+  private ApiKey refreshToken() {
+    return new ApiKey("RefreshJWT", "refreshToken", "header");
+  }
+
+  private SecurityContext securityContext() {
+    return SecurityContext.builder().securityReferences(defaultAuth()).build();
+  }
+
+  private List<SecurityReference> defaultAuth() {
+    AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+    AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+    authorizationScopes[0] = authorizationScope;
+    return List.of(new SecurityReference("accessJWT", authorizationScopes), new SecurityReference("refreshJWT", authorizationScopes));
   }
 }
