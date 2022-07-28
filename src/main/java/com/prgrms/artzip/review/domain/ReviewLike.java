35fa0525c9ed93a16d415f83d9aa40735056e10e
HyeonseoJUNG <windymay1920@gmail.com>
2022-07-28 18:38:18 +0900

    feat: review domain entity 작성

--- src/main/java/com/prgrms/artzip/review/domain/ReviewLike.java
+++ src/main/java/com/prgrms/artzip/review/domain/ReviewLike.java
@@ -1,5 +1,45 @@
 package com.prgrms.artzip.review.domain;
 
-public class ReviewLike {
+import com.prgrms.artzip.common.entity.BaseEntity;
+import com.prgrms.artzip.user.domain.User;
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.MapsId;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.JoinColumnOrFormula;
 
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class ReviewLike extends BaseEntity {
+
+  @EmbeddedId
+  private ReviewLikeId reviewLikeId;
+
+  @MapsId("reviewId")
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumnOrFormula(column =
+  @JoinColumn(name = "review_id",
+      referencedColumnName = "review_id")
+  )
+  private Review review;
+
+  @MapsId("userId")
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumnOrFormula(column =
+  @JoinColumn(name = "user_id",
+      referencedColumnName = "user_id")
+  )
+  private User user;
+
+  public ReviewLike(Review review, User user) {
+    this.reviewLikeId = new ReviewLikeId(review.getId(), user.getId());
+    this.review = review;
+    this.user = user;
+  }
 }
--- src/main/java/com/prgrms/artzip/review/domain/ReviewPhoto.java
+++ src/main/java/com/prgrms/artzip/review/domain/ReviewPhoto.java
@@ -1,5 +1,32 @@
 package com.prgrms.artzip.review.domain;
 
-public class ReviewPhoto {
+import com.prgrms.artzip.common.entity.BaseEntity;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class ReviewPhoto extends BaseEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "review_photo_id")
+  private Long id;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "review_id", referencedColumnName = "review_id")
+  private Review review;
+
+  @Column(name = "path", nullable = false, length = 2083)
+  private String path;
 }
