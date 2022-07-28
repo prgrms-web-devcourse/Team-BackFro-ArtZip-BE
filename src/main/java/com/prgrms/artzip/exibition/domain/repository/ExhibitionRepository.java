9fbf928568d3a43b34333cf39ae4e62b25c3434f
hong yuseok <jericoh@naver.com>
2022-07-29 04:31:13 +0900

    fix : 잘못된 클래스 타입 수정

--- src/main/java/com/prgrms/artzip/exibition/domain/repository/ExhibitionRepository.java
+++ src/main/java/com/prgrms/artzip/exibition/domain/repository/ExhibitionRepository.java
@@ -1,7 +1,8 @@
 package com.prgrms.artzip.exibition.domain.repository;
 
+import com.prgrms.artzip.exibition.domain.Exhibition;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ExhibitionRepository extends JpaRepository<Exception, Long> {
+public interface ExhibitionRepository extends JpaRepository<Exhibition, Long> {
 
 }
