20f823f562f5c9e8e54732341c5ccda25229ae56
parksang <hooo0503@gmail.com>
2022-07-28 17:34:27 +0900

    feat: Comment Enitity 기본 셋팅

--- src/main/java/com/prgrms/artzip/comment/domain/Comment.java
+++ src/main/java/com/prgrms/artzip/comment/domain/Comment.java
@@ -1,5 +1,27 @@
 package com.prgrms.artzip.comment.domain;
 
-public class Comment {
+import com.prgrms.artzip.common.entity.BaseEntity;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "comment")
+public class Comment extends BaseEntity {
+  @GeneratedValue
+  @Id
+  @Column(name = "comment_id")
+  private Long id;
+
+  @Column(name = "content", nullable = false, length = 500)
+  private String content;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "parent_id")
+  private Comment parent;
 }
