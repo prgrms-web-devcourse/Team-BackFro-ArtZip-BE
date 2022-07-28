7b1a0c4274d4c7ef597d208b0e93ccce3a904786
hong yuseok <jericoh@naver.com>
2022-07-29 00:14:21 +0900

    chore : 필요없는 에러 코드 제거, response에 code 추가

--- src/main/java/com/prgrms/artzip/common/ApiResponse.java
+++ src/main/java/com/prgrms/artzip/common/ApiResponse.java
@@ -3,19 +3,23 @@ package com.prgrms.artzip.common;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
 
 @Getter
 @JsonInclude(Include.NON_NULL)
 public class ApiResponse<T> {
   private String message;
+  private Integer code;
   private T data;
 
-  public ApiResponse(String message) {
+  public ApiResponse(String message, Integer code) {
     this.message = message;
+    this.code = code;
   }
 
-  public ApiResponse(String message, T data) {
+  public ApiResponse(String message, Integer code, T data) {
     this.message = message;
+    this.code = code;
     this.data = data;
   }
 }
