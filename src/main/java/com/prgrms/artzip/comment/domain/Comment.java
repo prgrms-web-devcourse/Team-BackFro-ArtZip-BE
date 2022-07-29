6ae0369991c00b8a683487a73ed60b5a921fc98a
park0503 <hooo0503@gmail.com>
2022-07-29 10:55:04 +0900

    feat: Comment Entity 셋팅 완료

--- src/main/java/com/prgrms/artzip/comment/domain/Comment.java
+++ src/main/java/com/prgrms/artzip/comment/domain/Comment.java
@@ -1,6 +1,9 @@
 package com.prgrms.artzip.comment.domain;
 
+import com.prgrms.artzip.common.ErrorCode;
 import com.prgrms.artzip.common.entity.BaseEntity;
+import com.prgrms.artzip.common.error.exception.InvalidRequestException;
+import com.prgrms.artzip.review.domain.Review;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -9,8 +12,14 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
+@Getter
 @Table(name = "comment")
 public class Comment extends BaseEntity {
   @GeneratedValue
@@ -24,4 +33,25 @@ public class Comment extends BaseEntity {
   @ManyToOne(fetch = FetchType.LAZY)
   @JoinColumn(name = "parent_id")
   private Comment parent;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "review_id")
+  private Review review;
+
+  @Builder
+  public Comment(String content, Comment parent, Review review) {
+    setContent(content);
+    this.parent = parent;
+    this.review = review;
+  }
+
+  private void setContent(String content) {
+    if (content == null || content.isBlank()) {
+      throw new InvalidRequestException(ErrorCode.CONTENT_IS_REQUIRED);
+    }
+    if (content.length() > 500) {
+      throw new InvalidRequestException(ErrorCode.CONTENT_IS_TOO_LONG);
+    }
+    this.content = content;
+  }
 }
