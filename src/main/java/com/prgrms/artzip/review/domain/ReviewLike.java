549eb6647958b63eb0fc69dd461561018424b90a
HyeonseoJUNG <windymay1920@gmail.com>
2022-07-29 11:08:41 +0900

    feat: review entity field 값 검증 추가

--- src/main/java/com/prgrms/artzip/review/domain/ReviewLike.java
+++ src/main/java/com/prgrms/artzip/review/domain/ReviewLike.java
@@ -8,12 +8,15 @@ import javax.persistence.FetchType;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.MapsId;
+import javax.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.JoinColumnOrFormula;
+import org.springframework.util.Assert;
 
 @Entity
+@Table(name = "review_like")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 public class ReviewLike extends BaseEntity {
@@ -38,8 +41,14 @@ public class ReviewLike extends BaseEntity {
   private User user;
 
   public ReviewLike(Review review, User user) {
+    validateFields(review, user);
     this.reviewLikeId = new ReviewLikeId(review.getId(), user.getId());
     this.review = review;
     this.user = user;
+  }
+
+  private void validateFields(Review review, User user) {
+    Assert.notNull(review, "review는 필수값입니다.");
+    Assert.notNull(user, "user는 필수값입니다.");
   }
 }
