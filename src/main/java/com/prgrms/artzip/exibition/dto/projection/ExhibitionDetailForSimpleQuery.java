ac3068bf9f75cfeaa6d1be883879e2f0ad4da3ea
hong yuseok <jericoh@naver.com>
2022-08-02 02:53:02 +0900

    fix : projection dto SuperBuilder 사용

--- src/main/java/com/prgrms/artzip/exibition/dto/projection/ExhibitionDetailForSimpleQuery.java
+++ src/main/java/com/prgrms/artzip/exibition/dto/projection/ExhibitionDetailForSimpleQuery.java
@@ -3,45 +3,23 @@ package com.prgrms.artzip.exibition.dto.projection;
 import com.prgrms.artzip.exibition.domain.Location;
 import com.prgrms.artzip.exibition.domain.Period;
 import com.prgrms.artzip.exibition.domain.enumType.Genre;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
 @Getter
 @NoArgsConstructor
-public class ExhibitionDetailForSimpleQuery {
-
-  private Long id;
+@SuperBuilder
+public class ExhibitionDetailForSimpleQuery extends ExhibitionBasicForSimpleQuery {
+  
   private Integer seq;
-  private String name;
   private Period period;
   private Genre genre;
   private String description;
   private Location location;
   private String inquiry;
   private String fee;
-  private String thumbnail;
   private String url;
   private String placeUrl;
   private long likeCount;
-
-  @Builder
-  public ExhibitionDetailForSimpleQuery(Long id, Integer seq, String name,
-      Period period, Genre genre, String description,
-      Location location, String inquiry, String fee, String thumbnail, String url,
-      String placeUrl, long likeCount) {
-    this.id = id;
-    this.seq = seq;
-    this.name = name;
-    this.period = period;
-    this.genre = genre;
-    this.description = description;
-    this.location = location;
-    this.inquiry = inquiry;
-    this.fee = fee;
-    this.thumbnail = thumbnail;
-    this.url = url;
-    this.placeUrl = placeUrl;
-    this.likeCount = likeCount;
-  }
 }
--- src/main/java/com/prgrms/artzip/exibition/dto/projection/ExhibitionForSimpleQuery.java
+++ src/main/java/com/prgrms/artzip/exibition/dto/projection/ExhibitionForSimpleQuery.java
@@ -1,28 +1,17 @@
 package com.prgrms.artzip.exibition.dto.projection;
 
 import com.prgrms.artzip.exibition.domain.Period;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
 @Getter
 @NoArgsConstructor
-public class ExhibitionForSimpleQuery {
-  private Long id;
-  private String name;
-  private String thumbnail;
+@SuperBuilder
+public class ExhibitionForSimpleQuery extends ExhibitionBasicForSimpleQuery {
+
   private Period period;
   private long likeCount;
   private long reviewCount;
-
-  @Builder
-  public ExhibitionForSimpleQuery(Long id, String name, String thumbnail,
-      Period period, long likeCount, long reviewCount) {
-    this.id = id;
-    this.name = name;
-    this.thumbnail = thumbnail;
-    this.period = period;
-    this.likeCount = likeCount;
-    this.reviewCount = reviewCount;
-  }
+  
 }
