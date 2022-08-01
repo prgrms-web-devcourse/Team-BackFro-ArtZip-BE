497d54c1cd3ad666969b3ae6bb17157c520771f9
hong yuseok <jericoh@naver.com>
2022-08-02 04:35:07 +0900

    refactor : ExhibitionSearchService 개선

--- src/main/java/com/prgrms/artzip/exibition/service/ExhibitionSearchService.java
+++ src/main/java/com/prgrms/artzip/exibition/service/ExhibitionSearchService.java
@@ -5,10 +5,10 @@ import static java.util.Objects.isNull;
 
 import com.prgrms.artzip.common.error.exception.InvalidRequestException;
 import com.prgrms.artzip.exibition.domain.repository.ExhibitionRepository;
-import com.prgrms.artzip.exibition.dto.projection.ExhibitionBasicForSimpleQuery;
-import com.prgrms.artzip.exibition.dto.projection.ExhibitionForSimpleQuery;
+import com.prgrms.artzip.exibition.dto.response.ExhibitionBasicInfo;
 import com.prgrms.artzip.exibition.dto.response.ExhibitionInfo;
 import java.util.List;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -23,18 +23,23 @@ public class ExhibitionSearchService {
   public Page<ExhibitionInfo> getExhibitionByQuery(String query, boolean includeEnd,
       Pageable pageable) {
     validateQuery(query);
-    Page<ExhibitionForSimpleQuery> exhibitionsPagingResult = exhibitionRepository.findExhibitionsByQuery(
-        query, includeEnd, pageable);
 
-    return exhibitionsPagingResult.map(ExhibitionInfo::new);
+    return exhibitionRepository.findExhibitionsByQuery(query, includeEnd, pageable)
+        .map(ExhibitionInfo::new);
   }
 
-  public List<ExhibitionBasicForSimpleQuery> getExhibitionsForReview(String query) {
+  public List<ExhibitionBasicInfo> getExhibitionsForReview(String query) {
     validateQuery(query);
-    return exhibitionRepository.findExhibitionsForReview(query);
+
+    return exhibitionRepository.findExhibitionsForReview(query).stream()
+        .map(exhibitionBasicForSimpleQuery -> ExhibitionBasicInfo.builder()
+            .exhibitionId(exhibitionBasicForSimpleQuery.getId())
+            .name(exhibitionBasicForSimpleQuery.getName())
+            .thumbnail(exhibitionBasicForSimpleQuery.getThumbnail())
+            .build())
+        .collect(Collectors.toList());
   }
-
-
+  
   private void validateQuery(String query) {
     if (isNull(query) || query.isBlank()) {
       throw new InvalidRequestException(EXHB_QUERY_BLANK);
