f3832991f63a18e8bf7e79ecf5c4dd3d90293ef4
parksang <hooo0503@gmail.com>
2022-08-02 18:27:11 +0900

    feat: CommentResponse에 childrenCount 및 대댓글 수에 따른 로직 변경

--- src/main/java/com/prgrms/artzip/comment/dto/response/CommentResponse.java
+++ src/main/java/com/prgrms/artzip/comment/dto/response/CommentResponse.java
@@ -4,6 +4,7 @@ import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.prgrms.artzip.comment.domain.Comment;
+import java.util.ArrayList;
 import java.util.List;
 import lombok.Getter;
 
@@ -12,9 +13,11 @@ import lombok.Getter;
 public class CommentResponse extends CommentInfo {
 
   private final List<CommentInfo> children;
+  private final Integer childrenCount;
 
   public CommentResponse(Comment entity, List<Comment> children) {
     super(entity);
-    this.children = children.stream().map(CommentInfo::new).toList();
+    this.childrenCount = children.size();
+    this.children = childrenCount < 10 ? children.stream().map(CommentInfo::new).toList() : new ArrayList<>();
   }
 }
