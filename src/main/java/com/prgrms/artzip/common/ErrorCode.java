dc70bfc985ede410e95b7c5ed1bad9a020a9efc4
HyeonseoJUNG <windymay1920@gmail.com>
2022-08-02 17:05:21 +0900

    fix : 겹치는 에러코드 수정

--- src/main/java/com/prgrms/artzip/common/ErrorCode.java
+++ src/main/java/com/prgrms/artzip/common/ErrorCode.java
@@ -18,10 +18,10 @@ public enum ErrorCode {
   INVALID_INPUT_VALUE(400, "C003", "적절하지 않은 값입니다."),
   NOT_FOUND(404, "C004", "해당 리소스를 찾을 수 없습니다."),
   BAD_REQUEST(400, "C005", "잘못된 요청입니다."),
-  MISSING_REQUEST_PARAMETER(400, "C005", "필수 파라미터가 누락되었습니다."),
-  INVALID_LENGTH(400, "C006", "올바르지 않은 길이입니다."),
+  MISSING_REQUEST_PARAMETER(400, "C006", "필수 파라미터가 누락되었습니다."),
+  INVALID_LENGTH(400, "C007", "올바르지 않은 길이입니다."),
   INVALID_FILE_EXTENSION(400, "C008", "올바르지 않은 파일 확장자입니다. (png, jpg, jpeg 가능)"),
-  MAX_UPLOAD_SIZE_EXCEEDED(400, "C008", "최대 파일 크기(5MB)보다 큰 파일입니다."),
+  MAX_UPLOAD_SIZE_EXCEEDED(400, "C009", "최대 파일 크기(5MB)보다 큰 파일입니다."),
 
   /**
    * User Domain
