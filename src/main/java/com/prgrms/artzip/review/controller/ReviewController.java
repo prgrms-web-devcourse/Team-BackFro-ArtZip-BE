9c0e965476ce7da6d3a909c069503b11038f2ae4
정현서(Hyeonseo Jung) <53249897+HyeonseoJUNG@users.noreply.github.com>
2022-08-04 17:26:24 +0900

    review 도메인 swagger 설명 (#65)
    * refactor : reviewController 반환 타입 명시
    
    반환 타입을 명시주어야 swagger에 제대로 표시됨
    
    * docs : swagger 설명 추가

--- src/main/java/com/prgrms/artzip/review/controller/ReviewController.java
+++ src/main/java/com/prgrms/artzip/review/controller/ReviewController.java
@@ -12,11 +12,10 @@ import com.prgrms.artzip.review.dto.response.ExhibitionsResponse;
 import com.prgrms.artzip.review.dto.response.ReviewCreateResponse;
 import com.prgrms.artzip.review.dto.response.ReviewLikeUpdateResponse;
 import com.prgrms.artzip.review.service.ReviewLikeService;
-import com.prgrms.artzip.review.dto.request.ReviewCreateRequest;
 import com.prgrms.artzip.review.dto.request.ReviewUpdateRequest;
-import com.prgrms.artzip.review.dto.response.ExhibitionsResponse;
 import com.prgrms.artzip.review.dto.response.ReviewIdResponse;
 import com.prgrms.artzip.review.service.ReviewService;
+import io.swagger.annotations.Api;
 import com.prgrms.artzip.user.domain.User;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
@@ -41,6 +40,7 @@ import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+@Api(tags = {"reviews"})
 @RestController
 @RequestMapping("/api/v1/reviews")
 @RequiredArgsConstructor
@@ -53,7 +53,7 @@ public class ReviewController {
 
   @ApiOperation(value = "후기 생성", notes = "후기 등록을 요청합니다.")
   @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-  public ResponseEntity<ApiResponse> createReview(
+  public ResponseEntity<ApiResponse<ReviewCreateResponse>> createReview(
       @RequestParam(value = "userId") Long userId,
       @ApiParam(value = "등록할 후기 데이터", required = true)
       @Parameter(name = "data", schema = @Schema(type = "string", format = "binary"))
@@ -71,7 +71,7 @@ public class ReviewController {
 
   @ApiOperation(value = "후기 작성 시, 전시회 검색", notes = "후기 작성 시, 전시회를 '전시회 이름'으로 검색합니다.")
   @GetMapping("/search/exhibitions")
-  public ResponseEntity<ApiResponse> getExhibitions(
+  public ResponseEntity<ApiResponse<ExhibitionsResponse>> getExhibitions(
       @ApiParam(value = "검색할 전시회 이름", required = true)
       @RequestParam(value = "query") String query) {
 
@@ -79,38 +79,41 @@ public class ReviewController {
         exhibitionSearchService.getExhibitionsForReview(query)
     );
 
+    ApiResponse apiResponse = ApiResponse.builder()
+        .message("전시회 검색 성공")
+        .status(HttpStatus.OK.value())
+        .data(response)
+        .build();
+
     return ResponseEntity.ok()
-        .body(ApiResponse.builder()
-            .message("전시회 검색 성공")
-            .status(HttpStatus.OK.value())
-            .data(response)
-            .build()
-        );
+        .body(apiResponse);
   }
 
   @ApiOperation(value = "후기 좋아요 등록/해제", notes = "후기 좋아요 등록/해제를 요청합니다.")
   @PatchMapping("{reviewId}/like")
-  public ResponseEntity<ApiResponse> updateReviewLike(
+  public ResponseEntity<ApiResponse<ReviewLikeUpdateResponse>> updateReviewLike(
       @RequestParam(value = "userId") final Long userId,
       @ApiParam(value = "좋아요 등록/해제할 후기의 ID")
       @PathVariable(value = "reviewId") final Long reviewId) {
 
     ReviewLikeUpdateResponse response = reviewLikeService.updateReviewLike(userId, reviewId);
 
+    ApiResponse apiResponse = ApiResponse.builder()
+        .message("후기 좋아요 등록/해제 성공")
+        .status(HttpStatus.OK.value())
+        .data(response)
+        .build();
+
     return ResponseEntity.ok()
-        .body(ApiResponse.builder()
-            .message("후기 좋아요 등록/해제 성공")
-            .status(HttpStatus.OK.value())
-            .data(response)
-            .build()
-        );
+        .body(apiResponse);
   }
   
   //TODO 아래 두 API 테스트 작성
 
-  @ApiOperation(value = "리뷰 댓글 다건 조회", notes = "리뷰의 댓글들을 조회합니다.")
+  @ApiOperation(value = "후기 댓글 다건 조회", notes = "후기의 댓글들을 조회합니다.")
   @GetMapping("/{reviewId}/comments")
   public ResponseEntity<ApiResponse<PageResponse<CommentResponse>>> getComment(
+      @ApiParam(value = "조회할 후기의 ID")
       @PathVariable Long reviewId,
       @PageableDefault Pageable pageable
   ) {
@@ -124,6 +127,7 @@ public class ReviewController {
   @ApiOperation(value = "리뷰 댓글 생성", notes = "리뷰에 댓글을 생성합니다.")
   @PostMapping("/{reviewId}/comments/new")
   public ResponseEntity<ApiResponse<CommentResponse>> createComment(
+      @ApiParam(value = "댓글 생성할 후기의 ID")
       @PathVariable Long reviewId,
       @RequestBody CommentCreateRequest request,
       @CurrentUser User user
@@ -141,6 +145,7 @@ public class ReviewController {
   @PatchMapping(value = "/{reviewId}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
   public ResponseEntity<ApiResponse> updateReview(
       @RequestParam(value = "userId") Long userId,
+      @ApiParam(value = "수정할 후기의 ID")
       @PathVariable(value = "reviewId") Long reviewId,
       @RequestPart(value = "data") ReviewUpdateRequest request,
       @RequestPart(required = false) List<MultipartFile> files) {
