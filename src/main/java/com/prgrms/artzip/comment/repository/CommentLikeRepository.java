b44fad9b4b957409ee1bbcd830cdbef50c5a6960
park0503 <hooo0503@gmail.com>
2022-08-07 19:30:19 +0900

    feat: CommentLikeRepository 생성

---
+++ src/main/java/com/prgrms/artzip/comment/repository/CommentLikeRepository.java
@@ -0,0 +1,17 @@
+package com.prgrms.artzip.comment.repository;
+
+import com.prgrms.artzip.comment.domain.CommentLike;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface CommentLikeRepository extends JpaRepository<CommentLike, Long> {
+  @Query("select cl from CommentLike cl where cl.user.id = :userId and cl.comment.id = :commentId")
+  Optional<CommentLike> findCommentLikeByCommentIdAndUserId(@Param("commentId") Long commentId, @Param("userId") Long userId);
+
+  @Modifying(clearAutomatically = true)
+  @Query("delete from CommentLike cl where cl.comment.id = :commentId and cl.user.id = :userId")
+  void deleteCommentLikeByCommentIdAndUserId(@Param("commentId") Long commentId, @Param("userId") Long userId);
+}
