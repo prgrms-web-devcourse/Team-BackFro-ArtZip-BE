82fe5e8dd7392c9a46b7827a1e8bd4c7f623d8f5
park0503 <hooo0503@gmail.com>
2022-08-07 19:29:47 +0900

    feat: Comment Like 복합 키 구조로 변경

--- src/main/java/com/prgrms/artzip/comment/domain/CommentLike.java
+++ src/main/java/com/prgrms/artzip/comment/domain/CommentLike.java
@@ -2,52 +2,50 @@ package com.prgrms.artzip.comment.domain;
 
 import static com.prgrms.artzip.common.ErrorCode.COMMENT_NOT_FOUND;
 import static javax.persistence.FetchType.LAZY;
-import static javax.persistence.GenerationType.IDENTITY;
 import static lombok.AccessLevel.PROTECTED;
 
 import com.prgrms.artzip.common.error.exception.NotFoundException;
 import com.prgrms.artzip.user.domain.User;
 import java.util.Objects;
-import javax.persistence.Column;
+import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.MapsId;
 import javax.persistence.Table;
-import javax.persistence.UniqueConstraint;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.JoinColumnOrFormula;
 
 @Getter
 @Entity
-@Table(name = "comment_like",
-    uniqueConstraints = {
-        @UniqueConstraint(
-            name = "unq_comment_id_user_id",
-            columnNames = {"comment_id", "user_id"}
-        )
-    })
+@Table(name = "comment_like")
 @NoArgsConstructor(access = PROTECTED)
 public class CommentLike {
-  @Id
-  @GeneratedValue(strategy = IDENTITY)
-  @Column(name = "comment_like_id")
-  private Long id;
 
+  @EmbeddedId
+  private CommentLikeId commentLikeId;
+
+  @MapsId("commentId")
   @ManyToOne(fetch = LAZY)
-  @JoinColumn(name = "comment_id")
+  @JoinColumnOrFormula(column =
+  @JoinColumn(name = "comment_id", referencedColumnName = "commentId")
+  )
   private Comment comment;
 
+  @MapsId("userId")
   @ManyToOne(fetch = LAZY)
-  @JoinColumn(name = "user_id")
+  @JoinColumnOrFormula(column =
+  @JoinColumn(name = "user_id", referencedColumnName = "commentId")
+  )
   private User user;
 
   @Builder
   public CommentLike(Comment comment, User user) {
     setComment(comment);
     setUser(user);
+    this.commentLikeId = new CommentLikeId(comment.getId(), user.getId());
   }
 
   private void setComment(Comment comment) throws NotFoundException {
