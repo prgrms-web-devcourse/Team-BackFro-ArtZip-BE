e175c06d978e1e434b2fd2785381e993484d5bc5
HyeonseoJUNG <windymay1920@gmail.com>
2022-08-08 11:05:38 +0900

    feat : 후기 조회 관련 DTO 수정

--- src/main/java/com/prgrms/artzip/review/dto/response/ReviewCommentInfo.java
+++ src/main/java/com/prgrms/artzip/review/dto/response/ReviewCommentInfo.java
@@ -1,14 +1,19 @@
 package com.prgrms.artzip.review.dto.response;
 
-import com.prgrms.artzip.comment.dto.response.CommentInfo;
-import com.prgrms.artzip.common.PageResponse;
+import com.prgrms.artzip.comment.dto.response.CommentResponse;
 import lombok.Getter;
 import lombok.experimental.SuperBuilder;
+import org.springframework.data.domain.Page;
 
 @SuperBuilder
 @Getter
 public class ReviewCommentInfo {
 
   private Long commentCount;
-  private PageResponse<CommentInfo> comments;
+  private Page<CommentResponse> comments;
+
+  public ReviewCommentInfo(Long commentCount, Page<CommentResponse> comments) {
+    this.commentCount = commentCount;
+    this.comments = comments;
+  }
 }
--- src/main/java/com/prgrms/artzip/review/dto/response/ReviewResponse.java
+++ src/main/java/com/prgrms/artzip/review/dto/response/ReviewResponse.java
@@ -1,15 +1,26 @@
 package com.prgrms.artzip.review.dto.response;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.prgrms.artzip.comment.dto.response.CommentResponse;
+import com.prgrms.artzip.review.domain.ReviewPhoto;
+import com.prgrms.artzip.review.dto.projection.ReviewWithLikeData;
+import com.prgrms.artzip.user.domain.User;
+import java.util.List;
 import lombok.Getter;
 import lombok.experimental.SuperBuilder;
+import org.springframework.data.domain.Page;
 
 @SuperBuilder
 @Getter
-@JsonInclude(Include.NON_NULL)
 public class ReviewResponse extends ReviewInfo {
 
   private ReviewUserInfo user;
   private ReviewExhibitionInfo exhibition;
+
+  public ReviewResponse(Long commentCount, Page<CommentResponse> comments,
+      ReviewWithLikeData reviewData, List<ReviewPhoto> photos,
+      User user, ReviewExhibitionInfo exhibition) {
+    super(commentCount, comments, reviewData, photos);
+    this.user = new ReviewUserInfo(user);
+    this.exhibition = exhibition;
+  }
 }
