c8d48fc64b533ecc863c058c091ef675c3b74839
HyeonseoJUNG <windymay1920@gmail.com>
2022-08-08 20:22:50 +0900

    feat : ReviewLikeRepository.findByReviewIdAndUserId() 추가

--- src/main/java/com/prgrms/artzip/review/domain/repository/ReviewLikeRepository.java
+++ src/main/java/com/prgrms/artzip/review/domain/repository/ReviewLikeRepository.java
@@ -2,15 +2,20 @@ package com.prgrms.artzip.review.domain.repository;
 
 import com.prgrms.artzip.review.domain.Review;
 import com.prgrms.artzip.review.domain.ReviewLike;
-import com.prgrms.artzip.review.domain.ReviewLikeId;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-public interface ReviewLikeRepository extends JpaRepository<ReviewLike, ReviewLikeId> {
+public interface ReviewLikeRepository extends JpaRepository<ReviewLike, Long> {
 
-    Long countByReview(Review review);
+  Long countByReview(Review review);
 
-    @Query("SELECT COUNT(rl) from ReviewLike rl WHERE rl.user.id = :userId")
-    Long countByUserId(@Param("userId") Long userId);
+  @Query("SELECT COUNT(rl) from ReviewLike rl WHERE rl.user.id = :userId")
+  Long countByUserId(@Param("userId") Long userId);
+
+  @Query("SELECT rl FROM ReviewLike rl"
+      + " WHERE rl.review.id = :reviewId and rl.user.id = :userId")
+  Optional<ReviewLike> findByReviewIdAndUserId(
+      @Param("reviewId") Long reviewId, @Param("userId") Long userId);
 }
