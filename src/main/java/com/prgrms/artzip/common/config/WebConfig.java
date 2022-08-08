954988f8d2c93ebaccdb93cd06dd73efd13359d3
park0503 <hooo0503@gmail.com>
2022-08-08 14:33:43 +0900

    feat: add Patch on cors

--- src/main/java/com/prgrms/artzip/common/config/WebConfig.java
+++ src/main/java/com/prgrms/artzip/common/config/WebConfig.java
@@ -32,7 +32,8 @@ public class WebConfig implements WebMvcConfigurer {
             HttpMethod.HEAD.name(),
             HttpMethod.POST.name(),
             HttpMethod.PUT.name(),
-            HttpMethod.DELETE.name())
+            HttpMethod.DELETE.name(),
+            HttpMethod.PATCH.name())
         .allowCredentials(true);
   }
 }
