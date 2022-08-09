39716f9c3e82adfdfa0ba0de51bb083141d28b1b
HyeonseoJUNG <windymay1920@gmail.com>
2022-08-09 18:20:51 +0900

    feat : 리뷰 다건 조회에 필요한 ReviewCustomRepositoy.findReviewsByExhibitionIdAndUserId() 구현

--- src/main/java/com/prgrms/artzip/review/domain/repository/ReviewCustomRepositoryImpl.java
+++ src/main/java/com/prgrms/artzip/review/domain/repository/ReviewCustomRepositoryImpl.java
@@ -1,20 +1,29 @@
 package com.prgrms.artzip.review.domain.repository;
 
+import static com.prgrms.artzip.comment.domain.QComment.comment;
 import static com.prgrms.artzip.review.domain.QReview.review;
 import static com.prgrms.artzip.review.domain.QReviewLike.reviewLike;
 import static com.prgrms.artzip.review.domain.repository.QuerydslUtils.alwaysFalse;
 import static com.prgrms.artzip.review.domain.repository.QuerydslUtils.nullSafeBooleanBuilder;
 
 import com.prgrms.artzip.review.domain.QReviewLike;
+import com.prgrms.artzip.review.dto.projection.ReviewWithLikeAndCommentCount;
 import com.prgrms.artzip.review.dto.projection.ReviewWithLikeData;
 import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.OrderSpecifier;
 import com.querydsl.core.types.Projections;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.core.types.dsl.CaseBuilder;
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
 import javax.persistence.EntityManager;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.support.PageableExecutionUtils;
 
 public class ReviewCustomRepositoryImpl implements ReviewCustomRepository {
 
@@ -57,6 +66,56 @@ public class ReviewCustomRepositoryImpl implements ReviewCustomRepository {
     return Optional.ofNullable(data);
   }
 
+  @Override
+  public Page<ReviewWithLikeAndCommentCount> findReviewsByExhibitionIdAndUserId(
+      Long exhibitionId, Long userId, Pageable pageable) {
+
+    List<ReviewWithLikeAndCommentCount> content = queryFactory.select(
+            Projections.fields(ReviewWithLikeAndCommentCount.class,
+                review.id.as("reviewId"),
+                review.date,
+                review.title,
+                review.content,
+                review.createdAt,
+                review.updatedAt,
+                new CaseBuilder()
+                    .when(alwaysFalse().or(reviewLikeUserIdEq(userId)))
+                    .then(true)
+                    .otherwise(false).as("isLiked"),
+                review.isPublic,
+                reviewLike.id.countDistinct().as("likeCount"),
+                comment.id.countDistinct().as("commentCount")
+            ))
+        .from(review)
+        .leftJoin(reviewLikeToGetIsLiked).on(reviewLikeToGetIsLiked.review.eq(review),
+            alwaysFalse().or(reviewLikeUserIdEq(userId)))
+        .leftJoin(reviewLike).on(review.id.eq(reviewLike.review.id))
+        .leftJoin(comment).on(review.id.eq(comment.review.id))
+        .where(review.isDeleted.eq(false),
+            review.isPublic.eq(true),
+            reviewExhibitionIdEq(exhibitionId))
+        .offset(pageable.getOffset())
+        .limit(pageable.getPageSize())
+        .groupBy(review.id, review.createdAt)
+        .orderBy(review.createdAt.desc())                     // 리뷰 생성 최신순
+//        .orderBy(reviewLike.id.countDistinct().desc())      // 리뷰 좋아요 많은 순
+//        .orderBy(comment.id.countDistinct().desc())         // 댓글 개수 많은 순
+        .fetch();
+
+    JPAQuery<Long> countQuery = queryFactory
+        .select(review.count())
+        .from(review)
+        .where(review.isDeleted.eq(false),
+            review.isPublic.eq(true),
+            reviewExhibitionIdEq(exhibitionId));
+
+    return PageableExecutionUtils.getPage(content, pageable, countQuery::fetchOne);
+  }
+
+
+  private BooleanBuilder reviewExhibitionIdEq(Long exhibitionId) {
+    return nullSafeBooleanBuilder(() -> review.exhibition.id.eq(exhibitionId));
+  }
 
   private BooleanBuilder reviewLikeUserIdEq(Long userId) {
     return nullSafeBooleanBuilder(() -> reviewLikeToGetIsLiked.user.id.eq(userId));
@@ -68,6 +127,12 @@ public class ReviewCustomRepositoryImpl implements ReviewCustomRepository {
     } else {
       return review.user.id.eq(userId).or(review.user.id.ne(userId).and(review.isPublic.eq(true)));
     }
+  }
+
+  // TODO: pageable의 sort 적용
+  private List<OrderSpecifier> getAllOrderSpecifiers(Pageable pageable) {
+
+    return null;
   }
 
 }
