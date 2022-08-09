87aa3b2189b5b0e00897511cc85503e30af6b8c5
park0503 <hooo0503@gmail.com>
2022-08-10 02:29:02 +0900

    feat: Exhibition create request dto 생성

---
+++ src/main/java/com/prgrms/artzip/exhibition/dto/request/ExhibitionCreateRequest.java
@@ -0,0 +1,20 @@
+package com.prgrms.artzip.exhibition.dto.request;
+
+import com.prgrms.artzip.exhibition.domain.enumType.Area;
+import com.prgrms.artzip.exhibition.domain.enumType.Genre;
+import java.time.LocalDate;
+import javax.validation.constraints.NotNull;
+import lombok.Getter;
+
+@Getter
+public record ExhibitionCreateRequest(@NotNull String name, String description,
+                                      @NotNull String fee,
+                                      Genre genre,
+                                      String inquiry, @NotNull String address,
+                                      @NotNull Area area,
+                                      @NotNull Double latitude, @NotNull Double longitude,
+                                      @NotNull String place, @NotNull LocalDate startDate,
+                                      @NotNull LocalDate endDate, String placeUrl,
+                                      @NotNull String thumbnail, String url) {
+
+}
