189aca3f94c1db67eeadb7af1cbd6fc9bec4b5e8
HyeonseoJUNG <windymay1920@gmail.com>
2022-08-09 18:33:55 +0900

    fix : 후기 다건 조회 response용 DTO 수정

--- src/main/java/com/prgrms/artzip/review/dto/response/ReviewInfo.java
+++ src/main/java/com/prgrms/artzip/review/dto/response/ReviewInfo.java
@@ -2,6 +2,7 @@ package com.prgrms.artzip.review.dto.response;
 
 import com.prgrms.artzip.comment.dto.response.CommentResponse;
 import com.prgrms.artzip.review.domain.ReviewPhoto;
+import com.prgrms.artzip.review.dto.projection.ReviewWithLikeAndCommentCount;
 import com.prgrms.artzip.review.dto.projection.ReviewWithLikeData;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -27,19 +28,19 @@ public class ReviewInfo extends ReviewCommentInfo {
   private Long likeCount;
   private List<ReviewPhotoInfo> photos;
 
-  public ReviewInfo(Long commentCount, Page<CommentResponse> comments,
-      ReviewWithLikeData reviewData, List<ReviewPhoto> photos) {
-    super(commentCount, comments);
-    this.reviewId = reviewData.getReviewId();
-    this.date = reviewData.getDate();
-    this.title = reviewData.getTitle();
-    this.content = reviewData.getContent();
-    this.createdAt = reviewData.getCreatedAt();
-    this.updatedAt = reviewData.getUpdatedAt();
-    this.isEdited = reviewData.getCreatedAt().isEqual(reviewData.getUpdatedAt()) ? false : true;
-    this.isLiked = reviewData.getIsLiked();
-    this.isPublic = reviewData.getIsPublic();
-    this.likeCount = reviewData.getLikeCount();
+  public ReviewInfo(ReviewWithLikeAndCommentCount review, List<ReviewPhoto> photos,
+      Page<CommentResponse> comments) {
+    super(review.getCommentCount(), comments);
+    this.reviewId = review.getReviewId();
+    this.date = review.getDate();
+    this.title = review.getTitle();
+    this.content = review.getContent();
+    this.createdAt = review.getCreatedAt();
+    this.updatedAt = review.getUpdatedAt();
+    this.isEdited = review.getCreatedAt().isEqual(review.getUpdatedAt()) ? false : true;
+    this.isLiked = review.getIsLiked();
+    this.isPublic = review.getIsPublic();
+    this.likeCount = review.getLikeCount();
     this.photos = photos.stream().map(ReviewPhotoInfo::new).collect(Collectors.toList());
   }
 }
--- src/main/java/com/prgrms/artzip/review/dto/response/ReviewsResponse.java
+++ src/main/java/com/prgrms/artzip/review/dto/response/ReviewsResponse.java
@@ -2,7 +2,7 @@ package com.prgrms.artzip.review.dto.response;
 
 import com.prgrms.artzip.comment.dto.response.CommentResponse;
 import com.prgrms.artzip.review.domain.ReviewPhoto;
-import com.prgrms.artzip.review.dto.projection.ReviewWithLikeData;
+import com.prgrms.artzip.review.dto.projection.ReviewWithLikeAndCommentCount;
 import com.prgrms.artzip.user.domain.User;
 import java.util.List;
 import org.springframework.data.domain.Page;
@@ -12,10 +12,9 @@ public class ReviewsResponse extends ReviewInfo {
   private ReviewUserInfo user;
   private ReviewExhibitionBasicInfoResponse exhibition;
 
-  public ReviewsResponse(Long commentCount, Page<CommentResponse> comments,
-      ReviewWithLikeData reviewData, List<ReviewPhoto> photos,
-      User user, ReviewExhibitionBasicInfoResponse exhibition) {
-    super(commentCount, comments, reviewData, photos);
+  public ReviewsResponse(ReviewWithLikeAndCommentCount review, Page<CommentResponse> comments,
+      List<ReviewPhoto> photos, User user, ReviewExhibitionBasicInfoResponse exhibition) {
+    super(review, photos, comments);
     this.user = new ReviewUserInfo(user);
     this.exhibition = exhibition;
   }
