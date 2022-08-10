7e475883d76d69ee40b1490a37830d7d8c5846b4
HyeonseoJUNG <windymay1920@gmail.com>
2022-08-10 11:30:59 +0900

    refactor : ReviewCustomRepositoryImpl에서 pageable의 sort 적용하도록 수정

---
+++ src/main/java/com/prgrms/artzip/review/domain/repository/ReviewSortType.java
@@ -0,0 +1,29 @@
+package com.prgrms.artzip.review.domain.repository;
+
+import static com.prgrms.artzip.comment.domain.QComment.comment;
+import static com.prgrms.artzip.review.domain.QReview.review;
+import static com.prgrms.artzip.review.domain.QReviewLike.reviewLike;
+
+import com.querydsl.core.types.Expression;
+import com.querydsl.core.types.Order;
+import com.querydsl.core.types.OrderSpecifier;
+
+public enum ReviewSortType {
+
+  CREATED_AT("createdAt", review.createdAt),
+  REVIEW_LIKE_COUNT("reviewLikeCount", reviewLike.id.countDistinct()),
+  COMMENT_COUNT("commentCount", comment.id.countDistinct());
+
+  private final String property;
+  private final Expression target;
+
+  ReviewSortType(String property, Expression target) {
+    this.property = property;
+    this.target = target;
+  }
+
+  public OrderSpecifier<?> getOrderSpecifier(Order direction) {
+    return new OrderSpecifier(direction, this.target);
+  }
+
+}
