978dc725757bcfed467bc507657f4f23fd9fc7f7
HyeonseoJUNG <windymay1920@gmail.com>
2022-08-10 10:18:28 +0900

    feat : ExhibitionService.getExhibition에 getReviews 추가

--- src/main/java/com/prgrms/artzip/exhibition/service/ExhibitionService.java
+++ src/main/java/com/prgrms/artzip/exhibition/service/ExhibitionService.java
@@ -5,7 +5,9 @@ import static com.prgrms.artzip.common.ErrorCode.INVALID_COORDINATE;
 import static com.prgrms.artzip.common.ErrorCode.INVALID_DISTANCE;
 import static org.springframework.util.StringUtils.hasText;
 
+import com.prgrms.artzip.common.ErrorCode;
 import com.prgrms.artzip.common.error.exception.InvalidRequestException;
+import com.prgrms.artzip.common.error.exception.NotFoundException;
 import com.prgrms.artzip.exhibition.domain.repository.ExhibitionRepository;
 import com.prgrms.artzip.exhibition.dto.projection.ExhibitionDetailForSimpleQuery;
 import com.prgrms.artzip.exhibition.dto.projection.ExhibitionForSimpleQuery;
@@ -13,18 +15,29 @@ import com.prgrms.artzip.exhibition.dto.projection.ExhibitionWithLocationForSimp
 import com.prgrms.artzip.exhibition.dto.response.ExhibitionAroundMeInfoResponse;
 import com.prgrms.artzip.exhibition.dto.response.ExhibitionDetailInfoResponse;
 import com.prgrms.artzip.exhibition.dto.response.ExhibitionInfoResponse;
+import com.prgrms.artzip.review.domain.Review;
+import com.prgrms.artzip.review.domain.ReviewPhoto;
+import com.prgrms.artzip.review.domain.repository.ReviewRepository;
+import com.prgrms.artzip.review.dto.projection.ReviewWithLikeAndCommentCount;
+import com.prgrms.artzip.review.dto.response.ReviewsResponseForExhibitionDetail;
+import com.prgrms.artzip.user.domain.User;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class ExhibitionService {
 
   private final ExhibitionRepository exhibitionRepository;
+  private final ReviewRepository reviewRepository;
 
   public Page<ExhibitionInfoResponse> getUpcomingExhibitions(Long userId,
       Pageable pageable) {
@@ -41,12 +54,13 @@ public class ExhibitionService {
     return exhibitionsPagingResult.map(ExhibitionInfoResponse::new);
   }
 
+  @Transactional(readOnly = true)
   public ExhibitionDetailInfoResponse getExhibition(Long userId, Long exhibitionId) {
     ExhibitionDetailForSimpleQuery exhibition = exhibitionRepository
         .findExhibition(userId, exhibitionId)
         .orElseThrow(() -> new InvalidRequestException(EXHB_NOT_FOUND));
 
-    // getReviews()
+    List<ReviewsResponseForExhibitionDetail> reviews = getReviews(userId, exhibitionId);
 
     return ExhibitionDetailInfoResponse.builder()
         .exhibitionId(exhibition.getId())
@@ -65,6 +79,7 @@ public class ExhibitionService {
         .lat(exhibition.getLocation().getLatitude())
         .lng(exhibition.getLocation().getLongitude())
         .isLiked(exhibition.getIsLiked())
+        .reviews(reviews)
         .build();
   }
 
@@ -101,4 +116,19 @@ public class ExhibitionService {
       throw new InvalidRequestException(INVALID_DISTANCE);
     }
   }
+
+  private List<ReviewsResponseForExhibitionDetail> getReviews(Long userId, Long exhibitionId) {
+    List<ReviewWithLikeAndCommentCount> reviews = reviewRepository.findReviewsByExhibitionIdAndUserId(
+        exhibitionId, Objects.isNull(userId) ? null : userId,
+        PageRequest.of(0, 4, Sort.by("reviewLikeCount").descending())).getContent();
+
+    return reviews.stream().map(r -> {
+      Review review = reviewRepository.findById(r.getReviewId())
+          .orElseThrow(() -> new NotFoundException(ErrorCode.REVIEW_NOT_FOUND));
+      List<ReviewPhoto> reviewPhotos = review.getReviewPhotos();
+      User reviewUser = review.getUser();
+      return new ReviewsResponseForExhibitionDetail(r, reviewPhotos, reviewUser);
+    }).collect(Collectors.toList());
+  }
+
 }
