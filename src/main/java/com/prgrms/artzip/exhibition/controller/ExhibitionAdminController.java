bc3f3a81890d61de5f2d010afa86ef538e414853
park0503 <hooo0503@gmail.com>
2022-08-12 01:07:42 +0900

    feat: 삭제 안된 전시회 전체 조회 관리자 api 구현

--- src/main/java/com/prgrms/artzip/exhibition/controller/ExhibitionAdminController.java
+++ src/main/java/com/prgrms/artzip/exhibition/controller/ExhibitionAdminController.java
@@ -1,13 +1,21 @@
 package com.prgrms.artzip.exhibition.controller;
 
+import com.prgrms.artzip.common.ApiResponse;
+import com.prgrms.artzip.common.PageResponse;
 import com.prgrms.artzip.exhibition.dto.request.ExhibitionCreateRequest;
+import com.prgrms.artzip.exhibition.dto.response.ExhibitionInfoResponse;
 import com.prgrms.artzip.exhibition.service.ExhibitionAdminService;
 import io.swagger.annotations.Api;
 import java.net.URI;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +28,7 @@ import org.springframework.web.multipart.MultipartFile;
 @RequiredArgsConstructor
 @RestController
 public class ExhibitionAdminController {
+
   private final ExhibitionAdminService exhibitionAdminService;
 
   @PostMapping()
@@ -29,5 +38,24 @@ public class ExhibitionAdminController {
   ) {
     Long exhibitionId = exhibitionAdminService.createExhibition(request, thumbnail);
     return ResponseEntity.created(URI.create("/api/v1/admin/exhibitions/" + exhibitionId)).build();
+  }
+
+  @GetMapping()
+  public ResponseEntity<ApiResponse<PageResponse<ExhibitionInfoResponse>>> getExhibitions(
+      @PageableDefault(
+          sort = {"created_at"},
+          direction = Direction.DESC
+      ) Pageable pageable
+  ) {
+    PageResponse<ExhibitionInfoResponse> exhibitions = PageResponse
+        .<ExhibitionInfoResponse>builder()
+        .page(exhibitionAdminService.getExhibitions((pageable)))
+        .build();
+    return ResponseEntity.ok(ApiResponse
+        .<PageResponse<ExhibitionInfoResponse>>builder()
+        .message("전시회 리스트 조회 완료")
+        .status(HttpStatus.OK.value())
+        .data(exhibitions)
+        .build());
   }
 }
