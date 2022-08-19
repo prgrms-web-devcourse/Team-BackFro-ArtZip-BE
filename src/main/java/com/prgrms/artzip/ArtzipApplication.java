f5d2bc609eee457df6bf6633736eeb7f96bba548
park0503 <hooo0503@gmail.com>
2022-08-20 05:07:53 +0900

    hotfix: timezone 변경

--- src/main/java/com/prgrms/artzip/ArtzipApplication.java
+++ src/main/java/com/prgrms/artzip/ArtzipApplication.java
@@ -9,12 +9,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing
 @SpringBootApplication
 public class ArtzipApplication {
-
-	@PostConstruct
-	public void started() {
-		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
-	}
-
 	public static void main(String[] args) {
 		SpringApplication.run(ArtzipApplication.class, args);
 	}
