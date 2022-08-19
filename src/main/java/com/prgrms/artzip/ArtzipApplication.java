4525053de0d325b71f238283dc8de544d108b3aa
park0503 <hooo0503@gmail.com>
2022-08-20 02:02:47 +0900

    fix: timezone 변경

--- src/main/java/com/prgrms/artzip/ArtzipApplication.java
+++ src/main/java/com/prgrms/artzip/ArtzipApplication.java
@@ -1,5 +1,7 @@
 package com.prgrms.artzip;
 
+import java.util.TimeZone;
+import javax.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -7,6 +9,12 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing
 @SpringBootApplication
 public class ArtzipApplication {
+
+	@PostConstruct
+	public void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(ArtzipApplication.class, args);
 	}
