717a80c139e45c7fb24fba5da4161889f90a238e
HyeonseoJUNG <windymay1920@gmail.com>
2022-08-22 04:15:37 +0900

    docs : ReviewCustomRepository에 설명 주석 추가

--- src/main/java/com/prgrms/artzip/review/domain/repository/ReviewCustomRepository.java
+++ src/main/java/com/prgrms/artzip/review/domain/repository/ReviewCustomRepository.java
@@ -7,14 +7,41 @@ import org.springframework.data.domain.Pageable;
 
 public interface ReviewCustomRepository {
 
+  /**
+   * Retrieves a review DTO by review id.
+   * @param reviewId must not be null
+   * @param userId id of the logged-in user
+   * @return the ReviewWithLikeAndCommentCount DTO
+   */
   Optional<ReviewWithLikeAndCommentCount> findReviewByReviewId(Long reviewId, Long userId);
 
+  /**
+   * returns all reviews(ReviewWithLikeAndCommentCount DTO) with the given parameters.
+   * @param exhibitionId
+   * @param userId id of the logged-in user
+   * @param pageable must not be null
+   * @return the list of ReviewWithLikeAndCommentCount DTO with pagination
+   */
   Page<ReviewWithLikeAndCommentCount> findReviews(
       Long exhibitionId, Long userId, Pageable pageable);
 
+  /**
+   * returns all reviews(ReviewWithLikeAndCommentCount DTO) with the given parameters.
+   * @param currentUserId id of the logged-in user
+   * @param targetUserId must not be null
+   * @param pageable must not be null
+   * @return the list of ReviewWithLikeAndCommentCount DTO with pagination
+   */
   Page<ReviewWithLikeAndCommentCount> findMyLikesReviews(
       Long currentUserId, Long targetUserId, Pageable pageable);
 
+  /**
+   * returns all reviews(ReviewWithLikeAndCommentCount DTO) with the given parameters.
+   * @param currentUserId id of the logged-in user
+   * @param targetUserId must not be null
+   * @param pageable must not be null
+   * @return the list of ReviewWithLikeAndCommentCount DTO with pagination
+   */
   Page<ReviewWithLikeAndCommentCount> findMyReviews(
       Long currentUserId, Long targetUserId, Pageable pageable);
 }
